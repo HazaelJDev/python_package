e9e1ec47328af42cad10854d1de73cb23c28c6bb
Hazael Jiménez <viajero25057@innovaccion.mx>
2022-05-31 17:06:15 -0500

    workshops file: Added Docstring with test

--- pycodigof/workshops.py
+++ pycodigof/workshops.py
@@ -1,6 +1,12 @@
 import requests
 
 def unreleased():
+    """Retorna los próximos talleres en CódigoFacilito.
+    
+    >>> type(unreleased()) == type(dict())
+    True
+    """
+
     response = requests.get('https://codigofacilito.com/api/v2/workshops/unreleased')
 
     if response.status_code == 200:
